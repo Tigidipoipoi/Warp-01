238f95b81d312d02af94e482f1a8deb3ce8d1d73
unknown <a.peyromaure@J002-0102-47.etudiant.rubika-edu.com.local>
2015-10-09 16:55:31 +0200

    1st commit.

---
+++ Assets/Scripts/TestLabScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using MiddleVR_Unity3D;
+using System;
+
+public class TestLabScript : MonoBehaviour
+{
+    // This method will be called when the object is selected by the wand and the wand's main button is pressed.
+    void VRAction()
+    {
+        vrCommand myCommand = new vrCommand("jambon", MyCommandHandler);
+        myCommand.Do("MyValue"); // || true || 1 || 1.1f || 1.2d ...
+    }
+
+    private vrValue MyCommandHandler(vrValue iValue)
+    {
+        return null;
+    }
+}
