1565c3907931aa9ca200f98e473a034afe7f8edc
AdrienPeyromaure <a.peyromauredebordbroca@rubika-edu.com>
2015-10-13 11:50:16 +0200

    Adds warp feedback.

---
+++ Assets/Scripts/ShuttleScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ShuttleScript : MonoBehaviour
+{
+    public Projector m_FeedBackProjector;
+
+    public void Start()
+    {
+        if (m_FeedBackProjector == null)
+        {
+            m_FeedBackProjector = transform.GetComponentInChildren<Projector>();
+        }
+        EnableFeedBackProjector(false);
+    }
+
+    public void EnableFeedBackProjector(bool enable = true)
+    {
+        if (m_FeedBackProjector == null)
+        {
+            Debug.LogError("ShuttleScript::EnableFeedBackProjector => No feedback projector is attached to the shuttle \"" + name + "\".");
+            return;
+        }
+
+        m_FeedBackProjector.enabled = enable;
+    }
+}
