dba9b3efa458652194f420657684fd11ba00063f
PolClarissou <p.clarissou@J002-0096-42.etudiant.rubika-edu.com.local>
2015-10-13 11:39:04 +0200

    dripping wall test with custom shader
    ugly af tho :dog:

---
+++ Assets/dripBlend.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dripBlend : MonoBehaviour {
+
+    SkinnedMeshRenderer skin;
+
+	void Start () {
+        skin = GetComponent<SkinnedMeshRenderer>();
+	}
+	
+	void Update () {
+        skin.SetBlendShapeWeight(0, 100f * Mathf.Sin(Time.time * 5f));
+        skin.SetBlendShapeWeight(1, 100f * Mathf.Sin(Time.time * 7f));
+        skin.SetBlendShapeWeight(2, 100f * Mathf.Sin(Time.time * 3f));
+        skin.SetBlendShapeWeight(3, 100f * Mathf.Sin(Time.time * 2f));
+    }
+}
