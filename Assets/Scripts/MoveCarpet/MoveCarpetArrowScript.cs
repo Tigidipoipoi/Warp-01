f90d27dd6ed0f7fc6a2c3fe3e770ed30e64768c6
AdrienPeyromaure <a.peyromauredebordbroca@rubika-edu.com>
2015-10-15 17:21:40 +0200

    WIP - Carpet move.

---
+++ Assets/Scripts/MoveCarpet/MoveCarpetArrowScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCarpetArrowScript : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider enteringCollider)
+    {
+        if (enteringCollider.tag == "Player")
+        {
+            Debug.Log("Entered carpet arrow \"" + name + "\".");
+        }
+    }
+}
