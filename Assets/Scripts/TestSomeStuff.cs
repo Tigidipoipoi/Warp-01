df6b0ec46a1fda90f1170c24229c6452f2260909
AdrienPeyromaure <a.peyromauredebordbroca@rubika-edu.com>
2015-10-26 14:15:29 +0100

    Prepare move modules for playtest.

--- Assets/Scripts/TestSomeStuff.cs
+++ Assets/Scripts/TestSomeStuff.cs
@@ -1,15 +1,28 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿//using UnityEngine;
+//using System.Collections;
 
 /// <summary>
 /// This script is made for test little functionnalities and will NOT stay in release.
 /// </summary>
-public class TestSomeStuff : MonoBehaviour
+public class TestSomeStuff : CleanBehaviourOnDestroy
 {
-    public IEnumerator Start()
-    {
-        yield return null;
-        Utils.ClearLog();
+    private VRInteractionNavigationWandJoystick m_JoystickNavigation;
 
+    public override void Start()
+    {
+        base.Start();
+
+        // We disable the move by joystick but keep the rotation.
+        m_JoystickNavigation = FindObjectOfType<VRInteractionNavigationWandJoystick>();
+        m_JoystickNavigation.EnableTranslation(false);
+        m_JoystickNavigation.EnableRotation(false);
+    }
+
+    public override void Die()
+    {
+        base.Die();
+
+        m_JoystickNavigation.EnableTranslation(true);
+        m_JoystickNavigation.EnableRotation(true);
     }
 }
