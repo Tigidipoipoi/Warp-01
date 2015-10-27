2afd8affd453e934a0c0b3f71236d8d59509b98a
Ken <x59kn16n@gmail.com>
2015-10-27 16:11:30 +0100

    Add wand Indicate manager.

---
+++ Assets/TEMPORAIRE/kenTest/KenAsett/Scripts/WandIndicateManager.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Warp01
+{
+    public class WandIndicateManager : MonoBehaviour
+    {
+        #region Members
+        private static VRWand theWand;
+        private static VRSelectionManager theSelectionManager;
+
+        public static VRWand TheWand
+        {
+            get
+            {
+                if (theWand == null)
+                {
+                    theWand = (VRWand)FindObjectOfType(typeof(VRWand));
+
+                    if (theWand == null)
+                    {
+                        Debug.LogError("An instance of " + typeof(VRWand) + " is needed in the scene, but there is none.");
+                    }
+                }
+                return theWand;
+            }
+        }
+        public static VRSelectionManager TheSelectionManager
+        {
+            get
+            {
+                if (theSelectionManager == null)
+                {
+                    theSelectionManager = (VRSelectionManager)FindObjectOfType(typeof(VRSelectionManager));
+
+                    if (theSelectionManager == null)
+                    {
+                        Debug.LogError("An instance of " + typeof(VRSelectionManager) + " is needed in the scene, but there is none.");
+                    }
+                }
+                return theSelectionManager;
+            }
+        }
+        #endregion Members
+
+        #region Unity events
+        // Update is called once per frame
+        void Update()
+        {
+            WandIndicateCheck();
+        }
+        #endregion Unity events
+
+        private void WandIndicateCheck()
+        {
+            if (null == TheSelectionManager.GetSelection())
+            {
+                WandHide();
+            }
+            else
+            {
+                WandIndicate();
+            }
+        }
+
+        private void WandIndicate()
+        {
+            TheWand.Show(true);
+        }
+
+        private void WandHide()
+        {
+            TheWand.Show(false);
+        }
+    }
+}
